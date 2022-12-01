08a847dc5412e8062820fc4508889627915244f8
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-12-01 16:41:05 +0100

    it make workey again. slow roll now

--- userpage.php
+++ userpage.php
@@ -3,22 +3,28 @@
 require 'auth.inc.php';
 require 'config.inc.php';
 
-echo 'Userpage, will be setuppage';
+echo 'Userpage, will be setup page';
 
 
     $LF_Pressure =              '';
+    $RF_Pressure =              '';
     
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['add'])) {
         $ok = true;
         if (!isset($_POST['LF_Pressure']) || $_POST['LF_Pressure'] === '') {
             $ok = false;
                 } else { $LF_Pressure = $_POST['LF_Pressure'];
         };
+        if (!isset($_POST['RF_Pressure']) || $_POST['RF_Pressure'] === '') {
+            $ok = false;
+                } else { $RF_Pressure = $_POST['RF_Pressure'];
+        };
         if ($ok) {
             //database go brrrrrr
             $db = new mysqli (MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_CARDATABASE);
             $sql = sprintf(
-                "INSERT INTO mx5globaldev (LF_pressure) VALUES ('%s')",
+                "INSERT INTO mx5globaldev (LF_pressure, RF_pressure) VALUES ('%s', '%s')",
+            $db->real_escape_string($LF_Pressure),
             $db->real_escape_string($LF_Pressure),
             );
         $db->query($sql);
@@ -40,12 +46,12 @@ echo 'Userpage, will be setuppage';
     Front Toe in: <input type="text"name="Front_Toe"> milimeters<br>
     Front Anti Rollbar: <input type="text"name="Front_ARB"> stiffness<br> -->
     Left Front Pressure: <input type= "text" name= "LF_Pressure"> Psi<br>
-   <!--  Left Front Spring Perch Offset: <input type="text"name="LF_SpringPerch"> milimeters<br>
+<!--     Left Front Spring Perch Offset: <input type="text"name="LF_SpringPerch"> milimeters<br>
     Left Front Bump Stiffness: <input type="text"name="LF_Bumpstiffness"> clicks<br>
     Left Front Rebound Stiffnes: <input type="text"name="LF_ReboundStiffness"> clicks<br>
-    Left Front Camber: <input type="text"name="LF_Camber"> milimeters<br>
+    Left Front Camber: <input type="text"name="LF_Camber"> milimeters<br> -->
     Right Front Pressure: <input type="text"name="RF_Pressure"> Psi<br>
-    Right Front Spring Perch Offset: <input type="text"name="RF_SpringPerch"> milimeters<br>
+    <!-- Right Front Spring Perch Offset: <input type="text"name="RF_SpringPerch"> milimeters<br>
     Right Front Bump Stiffness: <input type="text"name="RF_BumpStiffness"> clicks<br>
     Right Front Rebound Stiffnes: <input type="text"name="RF_ReboundStiffness"> clicks<br>
     Right Front Camber: <input type="text"name="RF_Camber"> milimeters<br>
@@ -62,5 +68,5 @@ echo 'Userpage, will be setuppage';
     Rear Toe in: <input type="text"name="Rear_toe"> milimeters<br>
     Rear Anti Rollbar: <input type="text"name="Rear_ARB"> stiffness<br> -->
 </select><br>
-<input type="submit" name="add setup" value="Add setup"><br>
+<input type="submit" name="add" value="Add setup"><br>
 </form>
