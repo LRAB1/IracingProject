d725923ea34cfee72c34fb55a079df9b1b40537c
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-12-09 16:17:48 +0100

    bangerang

--- averagesetup.php
+++ averagesetup.php
@@ -11,6 +11,35 @@ echo 'Under construction';
 
 $sql = sprintf ("SELECT AVG(LF_Pressure), AVG(`RF_pressure`), AVG(`LF_SpringPerchOffset`), AVG(`Front_Toe`) from mx5globaldev;"); */
 ?>
+
+<?php
+
+
+// Connect to the database
+$connection = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_CARDATABASE);
+
+// Check for errors
+if (mysqli_connect_errno()) {
+    die('Failed to connect to the database: ' . mysqli_connect_error());
+}
+
+// Execute the query
+$result = mysqli_query($connection, 'SELECT AVG(RF_pressure) from mx5globaldev');
+
+// Check for errors
+if (!$result) {
+    die('Failed to execute the query: ' . mysqli_error($connection));
+}
+
+// Loop over the results and print each row
+while ($row = mysqli_fetch_assoc($result)) {
+    print_r($row);
+}
+
+// Close the connection
+mysqli_close($connection);
+?>
+
 <form
     action=""
     method="post">
