9fbe1ad0bed6d2ddd4e12bfad1bc0ba74466bea6
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2023-09-08 11:54:46 +0200

    testing setup config.inc

--- src/config.inc.php
+++ src/config.inc.php
@@ -3,8 +3,8 @@
 define ('MYSQL_USER', getenv('MYSQL_USER'));
 define ('MYSQL_PASSWORD', getenv('MYSQL_PASSWORD'));
 define ('MYSQL_HOST', getenv('MYSQL_HOST'));
-define ('MYSQL_CARDATABASE', 'cars');
-define ('MYSQL_DATABASE', getenv('php)'));
-define ('MYSQL_USERDATABASE', 'users');
+define ('MYSQL_CARDATABASE', 'SetupProject.cars');
+define ('MYSQL_DATABASE', getenv('SetupProject)'));
+define ('MYSQL_USERDATABASE', 'SetupProject.users');
 
 ?>
