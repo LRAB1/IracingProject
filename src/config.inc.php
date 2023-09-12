8fd5e651b7880cfcf69f4dd271edd5e17c4478be
LRAB1 <lexbant@gmail.com>
2023-09-12 19:35:08 +0200

    setting up env variable

--- src/config.inc.php
+++ src/config.inc.php
@@ -4,7 +4,7 @@ define ('MYSQL_USER', getenv('MYSQL_USER'));
 define ('MYSQL_PASSWORD', getenv('MYSQL_PASSWORD'));
 define ('MYSQL_HOST', getenv('MYSQL_HOST'));
 define ('MYSQL_CARDATABASE', 'SetupProject.cars');
-define ('MYSQL_DATABASE', getenv('SetupProject)'));
+define ('MYSQL_DATABASE', getenv('MYSQL_DATABASE)'));
 define ('MYSQL_USERDATABASE', 'SetupProject.users');
 
 ?>
